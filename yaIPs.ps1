bcb81506e72547757abd85b0f4cc3144709ea468
fhAnso <felix.hansow@proton.me>
2024-06-26 12:54:20 +0200

    INITIAL COMMIT
    The script currently decides which network potion it wants to use. If a network potion is analyzed as the first argument, this setting is used, otherwise the address of the current computer is used.

---
+++ yaIPs.ps1
@@ -0,0 +1,46 @@
+if ($args.Length -eq 0) 
+{
+    #$local_ip = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -ne "127.0.0.1" }).IPAddress
+    $local_ip = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -ne "127.0.0.1" -and $_.AddressState -eq "Preferred" }).IPAddress[0]
+    $network_portion = $local_ip.Substring(0, $local_ip.LastIndexOf('.'))
+
+    Write-Output "No network portion given: using current address.."
+} 
+else 
+{
+    $network_portion = $args[0]
+}
+
+$output_file = "temp.txt"
+
+Write-Output "Listing of reachable IPv4 addresses.."
+Write-Output ("NOTE: The addresses will be saved to the following file: `"$output_file`"")
+
+Remove-Item $output_file -ErrorAction SilentlyContinue
+
+function Test-Host 
+{
+    param 
+    (
+        [string]$Address
+    )
+
+    $pingResult = Test-Connection -ComputerName $Address -Count 1 -Quiet
+
+    if ($pingResult) 
+    {
+        $Address | Out-File -Append -FilePath $output_file
+    }
+}
+
+$jobs = @()
+
+for ($host_portion = 1; $host_portion -le 254; $host_portion++) 
+{
+    $address = "$network_portion.$host_portion"
+    $jobs += Start-Job -ScriptBlock { param($addr) Test-Host -Address $addr } -ArgumentList $address
+}
+
+$jobs | ForEach-Object { $_ | Wait-Job | Remove-Job }
+
+Write-Output "Done"
